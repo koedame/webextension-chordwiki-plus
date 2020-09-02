6502282a9c0f7da17c0b7c966d8a24724b2aa2fb
💩 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-09-02 19:24:37 +0900

    スクロール速度の設定を変更

--- src/content_script/components/ChangeAutoScrollSpeedButton.vue
+++ src/content_script/components/ChangeAutoScrollSpeedButton.vue
@@ -1,18 +1,14 @@
 <template lang="pug">
 .chordwiki-plus-change-auto-scroll-speed-button
   .vertical-buttons
-    //- TODO 直線的な変化ではなくてカーブをつくる
-    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 400", type="is-info", size="is-small", @click="changeSpeed(400)") 1
-    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(400)") 1
+    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 300", type="is-info", size="is-small", @click="changeSpeed(300)") 1
+    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(300)") 1
 
-    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 300", type="is-info", size="is-small", @click="changeSpeed(300)") 2
-    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(300)") 2
+    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 200", type="is-info", size="is-small", @click="changeSpeed(200)") 2
+    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(200)") 2
 
-    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 200", type="is-info", size="is-small", @click="changeSpeed(200)") 3
-    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(200)") 3
-
-    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 150", type="is-info", size="is-small", @click="changeSpeed(150)") 3.5
-    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(150)") 3.5
+    b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 150", type="is-info", size="is-small", @click="changeSpeed(150)") 3
+    b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(150)") 3
 
     b-button.vertical-buttons__item(v-if="$store.state.autoScroll.scrollSpeed === 100", type="is-info", size="is-small", @click="changeSpeed(100)") 4
     b-button.vertical-buttons__item(v-else, size="is-small", @click="changeSpeed(100)") 4
