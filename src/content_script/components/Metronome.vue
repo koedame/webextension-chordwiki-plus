fb7afff12a379f487303f3001e77f74c3f076d4c
💩 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-09-06 17:51:59 +0900

    簡易メトロノーム作成

---
+++ src/content_script/components/Metronome.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+span#chordwiki-plus-metronome
+  span.clock-hands
+    span(v-show="metronomeTick")
+      | \
+  span.clock-hands
+    span(v-show="!metronomeTick")
+      | /
+</template>
+
+<script>
+export default {
+  props: {
+    bpm: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      meteonomeTimer: null,
+      count: 0,
+    };
+  },
+  mounted() {
+    const metronomeInterval = 60000 / this.bpm;
+    this.meteonomeTimer = setInterval(this.countUp, metronomeInterval);
+  },
+
+  computed: {
+    metronomeTick() {
+      return this.count % 2;
+    },
+  },
+
+  methods: {
+    countUp() {
+      this.count++;
+    },
+  },
+
+  destroyed() {
+    if (this.meteonomeTimer) {
+      clearInterval(this.meteonomeTimer);
+    }
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.clock-hands
+  display: inline-block
+  width: 10px
+  color: #888
+</style>
